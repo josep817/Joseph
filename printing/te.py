ced32a8ff9e7e7377dedd73d09c0415f858ff080
josep817 <nevillej502@gmail.com>
2026-02-03 16:50:45 +0300

    Add files via upload
    How to use f_string and more

---
+++ printing/te.py
@@ -0,0 +1,43 @@
+"""
+> A function
+> is any name followed by a braccket 
+Example:
+         
+        name()
+        This type of function is called a "function call".
+
+        The term "argument" represent any value that is supplied to the function call.
+ """       
+
+print("Total number of mangoes", 45) # 45 is an argument passed to the function call.
+
+print("Total money", 700)
+
+print(556.95)
+
+print("hello world") 
+
+result=68
+print(result)
+
+"""
+f-string substitution
+
+print(f"text{variable})
+
+"""
+lenth = 40
+width =25
+print(f"{lenth}x{width} = {lenth*width}")
+
+print(f"The lenth of a rectangle is {width}")
+ 
+"""
+str.format()
+print("{index}".format(variable))
+"""
+
+print ("The width is {}".format(width))
+
+
+print("{}x{}={}". format(lenth,width,lenth*width))
